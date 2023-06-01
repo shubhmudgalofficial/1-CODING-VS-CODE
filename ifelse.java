8f82dbb68590ac840e4d142a47a2bc120bc2d7d9
shubhmudgalofficial <shubhmudgalofficial@gmail.com>
2023-06-01 16:50:40 +0530

    if else program 😊

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+
+
+
+public class ifelse {
+    public static void main(String[] args) {
+        int age ;
+        age=18;
+        if(age>=18&&age<=60){
+            System.out.println("u can drive");
+        }
+        else{
+            System.out.println("u cant drive");
+        }
+    }
+   
+    
+}
+
